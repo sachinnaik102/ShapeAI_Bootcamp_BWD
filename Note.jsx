8d061faeffc201c034fc2208c3c3bfdef745977d
sachinnaik102 <75070861+sachinnaik102@users.noreply.github.com>
2021-06-06 15:52:36 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+function Note() {
+  return (
+    <div className="note">
+      <h1> Javascript and React.js</h1>
+      <p>
+        i have attended the 7-days bootcamp which was being arranged by ShapeAI
+        and the classes were taken by Mr.Shaurya Sinha,covered all basics of
+        html,javascript and React.js .
+      </p>
+      <p>
+        I am sachin Naik ,very passionate towards cricket.Till now never missed
+        any of indian cricket matches and IPL matches too..
+      </p>
+    </div>
+  );
+}
+export default Note;
